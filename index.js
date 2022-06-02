2ab8d682c8344f54f65c3101aa3593a4e6d87448
Julio Villalvazo <juliovillalvazo26@gmail.com>
2022-06-01 21:14:29 -0500

    added server function

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
+require('dotenv').config();
 const express = require('express');
+const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
-const app = express();
-
-let persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456',
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-5323523',
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-234345',
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendieck',
-        number: '39-23-6423122',
-    },
-];
+const Person = require('./models/person');
 
 morgan.token('dataSent', function dataSent(req) {
     const data = JSON.stringify(req.body);
@@ -43,13 +22,15 @@ app.use(
 app.use(cors());
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    Person.find({}).then((people) => {
+        res.json(people);
+    });
 });
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = +req.params.id;
-    const person = persons.find((p) => p.id === id);
-    person ? res.json(person) : res.status(404).end();
+    Person.findById(request.params.id).then((person) => {
+        res.json(person);
+    });
 });
 
 app.get('/info', (req, res) => {
@@ -73,28 +54,29 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
     const body = req.body;
+    console.log(body);
 
     if (!body || !body.name || !body.number) {
         return res.status(400).json({ error: 'the name or number is missing' });
     }
 
-    if (persons.find((person) => person.name === body.name)) {
-        return res.status(400).json({
-            error: 'name must be unique',
-        });
-    }
+    // if (persons.find((person) => person.name === body.name)) {
+    //     return res.status(400).json({
+    //         error: 'name must be unique',
+    //     });
+    // }
 
-    const newEntry = {
+    const newEntry = new Person({
         name: body.name,
         number: body.number,
-        id: Math.floor(Math.random() * 1000000),
-    };
+    });
 
-    persons = persons.concat(newEntry);
-    res.json(newEntry);
+    newEntry.save().then((savedPerson) => {
+        res.json(savedPerson);
+    });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
